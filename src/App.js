c0379f8604ae2465d2712e145aadfb1ecb35e674
Harmit1708 <Harmitsonani@gmail.com>
2022-01-20 09:59:51 +0530

    https://github.com/Harmit1708/sb-admin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Sidebar from './components/Sidebar'
+import Dashboard from './components/Dashboard'
+import Allstudents from './components/Allstudents'
+import Addstudents from './components/Addstudents'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import {useState} from 'react'
+import EditStudent from "./components/EditStudent"
 
 function App() {
+
+  let data = {
+    earning: "40,000",
+    annual: "2,40,000",
+    task: 20,
+    pending: 26,
+  };
+
+  let [students, setStudents] = useState([
+    {
+      name: "Harmit",
+      email: "Harmit@gmail.com",
+      mobile: "12345678",
+      class: "B30WD",
+    },
+    {
+      name: "Hemant",
+      email: "Hemant@gmail.com",
+      mobile: "12345678",
+      class: "B30WD",
+    },
+    {
+      name: "Raj",
+      email: "Raj@gmail.com",
+      mobile: "12345678",
+      class: "B30WD",
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div style={{ display: "grid", gridTemplateColumns: "13% 85%" }}>
+        <div>
+          <Sidebar />
+        </div>
+        <div>
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard data={data} />} />
+            <Route path="/all-students" element={<Allstudents data ={{students,setStudents}} />} />
+            <Route path="/add-students" element={<Addstudents data={{students,setStudents}} />} />
+            <Route path="/edit-student/:id" element={<EditStudent data={{students,setStudents}} />} />
+            <Route path="/" element={<Dashboard data={data} />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Addstudents.js
@@ -0,0 +1,74 @@
+import React,{useState} from "react"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import {useNavigate} from 'react-router-dom'
+
+function Addstudents(props) {
+  let navigate = useNavigate();
+  let [name,setName] = useState("");
+  let [email,setEmail] = useState("");
+  let [mobile,setMobile] = useState("");
+  let [cls,setCls] = useState("");
+
+let handlesubmit = () => {
+  let newData  = {name,email,mobile,"class":cls};
+  let newArr = [...props.data.students]
+  newArr.push(newData);
+  props.data.setStudents(newArr)
+
+  navigate("/all-");
+}
+
+  return (
+    <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          value={name}
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          value={email}
+          type="email"
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Mobile</Form.Label>
+        <Form.Control
+          value={mobile}
+          type="Number"
+          placeholder="Mobile"
+          onChange={(e) => setMobile(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Class</Form.Label>
+        <Form.Control
+        value={cls}
+          type="text"
+          placeholder="Class"
+          onChange={(e) => setCls(e.target.value)}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit" onClick={handlesubmit}>
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default Addstudents;
